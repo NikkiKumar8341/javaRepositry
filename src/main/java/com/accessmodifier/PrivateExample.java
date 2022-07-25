c2d95d68c25636d1d916d4846a90c8c7d938a1bf
javafreasher <sandeep.mallidi@motivitylabs.com>
2022-07-25 17:04:14 +0530

    Day-8

---
+++ src/main/java/com/accessmodifier/PrivateExample.java
@@ -0,0 +1,11 @@
+package com.accessmodifier;
+class Employee{
+    private void setName(String name){
+        System.out.println("name "+name);
+    }
+}
+public  class PrivateExample{
+     public static void main(String[] args) {
+
+     }
+ }
